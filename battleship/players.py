7a921d46dcf5d164ba5d78d9215f0ef3e939e2b7
dcreekp <dcreekp@gmail.com>
2014-11-28 18:01:08 +0900

    moving setup from engine to players

--- battleship/players.py
+++ battleship/players.py
@@ -128,6 +128,71 @@ class Human(Player):
     def name(self):
         return "Player 1"
 
+    def human_setup(self):
+        """ prompts the user to set up their board; manually choose which ship
+            and hide it with hide_ships(), or automatically hide all
+        """
+        # dict of all ship's for player's board
+        fleet = self.brd.fleet
+        # list of ship objects left to hide
+        fleet_lst = [fleet[ship] for ship in fleet]
+        shuffle(fleet_lst)
+
+
+        while len(fleet_lst) > 0:
+            print(PROMPT['border'])
+
+            # displays the current board
+            show_board(self.brd)
+
+            # prompts user to select a ship to hide or autohide the rest
+            select = input(PROMPT['which_ship'].format('\n   '.join([str(ship)
+                                                        for ship in fleet_lst])))
+
+            if select.lower() == 'a': # automates the hiding process
+                for ship in fleet_lst:
+                    self.auto_hide_ships(ship, 1)
+                self._confirm_setup()
+                return
+
+            # for manually hiding the selected ship
+            if fleet.get(select.upper()) in fleet_lst:
+                check = self.hide_ships(fleet.get(select.upper()))
+                if check == True:
+                    # only removes if ship was hidden successfully
+                    fleet_lst.remove(fleet.get(select.upper()))
+                else: # if hide_ships() returns None start again
+                    continue
+            # in case user just types <Enter> will pop off first in fleet_lst
+            elif select == '':
+                self.hide_ships(fleet_lst.pop(0))
+            # in case user selects a ship already hidden
+            elif fleet.get(select.upper()) not in fleet_lst and\
+                                select.upper() in 'KTSYP':
+                print(PROMPT['already_hidden'].format(str(fleet.get(select.upper(
+                    )))))
+            else:
+                print(PROMPT['which_ship_explain'])
+
+        self._confirm_setup()
+
+    def _confirm_setup(self):
+        """display the completed board setup for player to confirm or revise"""
+
+        show_board(self.brd)
+        check = input(PROMPT['good2go'])
+
+        if check == '':
+            return
+        elif check[0].lower() == 'n':
+            print(PROMPT['start_again'])
+            self.brd.remove_fleet()
+            return self.human_setup()
+        else:
+            return
+
+
+
     def hide_ships(self, ship):
         """ prompts the user to select head using pick_coord() and tail using
             _full() to hide a ship, and using _head2tail() to show possible
@@ -238,14 +303,14 @@ class Human(Player):
             if new:
                 break
 
-        if ask == 'hide_head' and new[0] in self.occupied:
+        if ask == 'hide_head' and new in self.occupied:
             # new coord rejected for hiding if it overlaps
             print(PROMPT['occupied'])
             return None
         if ask == 'where2bomb':
-            print(PROMPT['player_attack'].format(new[1]))
+            print(PROMPT['player_attack'].format(convert(new)))
 
-        return new[0]
+        return new
 
 
 class Computer(Player):
@@ -260,6 +325,20 @@ class Computer(Player):
 
     def name(self):
         return "Computer"
+
+    def comp_setup(self):
+        """gets computer to hide the ships for game play"""
+
+        # a dict for all the ships on comp's board
+        fleet = self.brd.fleet
+        # list of ship objects left to hide
+        fleet_lst = [fleet[ship] for ship in fleet]
+        shuffle(fleet_lst)
+
+        print(PROMPT['border'])
+
+        for ship in fleet_lst:
+            self.auto_hide_ships(ship)
 
     def random_pick(self):
         """ computer randomly selects a coordinate to bomb out of a list of
